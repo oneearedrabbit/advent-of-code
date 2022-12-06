daa9ad464480ea51253729257f3171cd70d99dbd
oneearedrabbit <oneearedrabbit@users.noreply.github.com>
2022-12-05 23:29:23 -0500

    Create 05.rb

---
+++ 2022/05/05.rb
@@ -0,0 +1,25 @@
+def read
+  data, commands = File.read('input').split("\n\n")
+  data = data.split("\n")[0..-2].
+           map { |el| el.scan(/..../) }.transpose.
+           map { |el| el.map { |el| el.strip && el.scan(/\w/) }.delete_if(&:empty?).flatten.reverse }
+  commands = commands.split("\n").map { |el| el.split.map(&:to_i).select { |el| el > 0 } }.
+               map { |n, m, k| [n, m-1, k-1] }
+  [data, commands]
+end
+
+data, commands = *read
+commands.each do |num, from, to|
+  data[to] = data[to] + data[from].last(num).reverse
+  data[from] = data[from][..-num-1]
+end
+
+puts data.map(&:last).join  # => JCMHLVGMG
+
+data, commands = *read
+commands.each do |num, from, to|
+  data[to] = data[to] + data[from].last(num)
+  data[from] = data[from][..-num-1]
+end
+
+puts data.map(&:last).join  # => LVMRWSSPZ
